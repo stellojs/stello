fd8c2ab9d04cc7530609ca9e0cc9dafd5d184010
jtrussell <jus.russell@gmail.com>
2016-02-01 17:34:50 -0500

    feature: Add "cardsInList" helper

--- tpl/src/helpers/eachCardInList.js
+++ tpl/src/helpers/eachCardInList.js
@@ -1,7 +1,22 @@
 
 'use strict';
 
-module.exports = function() {
-  // silence is golden
+module.exports = function(listId, options) {
+  var ret = ''
+    , listIdNameMap = {};
+
+  (this.allLists || []).forEach(function(l) {
+    listIdNameMap[l.id] = l.name;
+  });
+
+  (this.allCards || [])
+    .filter(function(c) {
+      return listId === c.idList || listId === listIdNameMap[c.idList];
+    })
+    .forEach(function(c) {
+      ret += options.fn(c);
+    });
+
+  return ret;
 };
 
