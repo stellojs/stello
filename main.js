fcb7d78cd08787fddd4ad9f4b304be1f737b2399
jtrussell <jus.russell@gmail.com>
2016-02-11 14:15:12 -0500

    feature: Working forms

--- main.js
+++ main.js
@@ -10,8 +10,15 @@
 'use strict';
 
 $('.the-public-key').on('change keyup',function() {
-  var value = this.value;
-  console.log(value);
+  var value = this.value
+    , $theTokenLink = $('.the-token-link')
+  if(value) {
+    $theTokenLink.removeClass('disabled');
+    $theTokenLink.attr('href', 'https://trello.com/1/authorize?key=' + value + '&name=stello&scope=read&expiration=never&response_type=token');
+  } else {
+    $theTokenLink.addClass('disabled');
+    $theTokenLink.attr('href', 'javascript:void(0)');
+  }
 });
 
 }(jQuery));
